1d6ae6dab0000934f8b6aa303dd62712699f873f
Keith Ehlis <keith@ehlis.com>
2017-11-10 20:08:36 -0600

    Done with "   Async Pipe "as" syntax and routing" training module.

---
+++ src/health/meals/containers/meal/meal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meal',
+    styleUrls: ['meal.component.scss'],
+    template: `
+        <div>
+            New Meal            
+        </div>
+`
+})
+export class MealComponent {
+    constructor() {}
+}
