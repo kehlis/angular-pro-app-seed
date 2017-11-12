ff39b3386aa0a750bc5046882254389e86c1579a
Keith Ehlis <keith@ehlis.com>
2017-11-12 17:11:41 -0600

    Finished "Custom Workout / Meal Pipes" module.

--- src/health/workouts/components/workout-form/workout-form.component.ts
+++ src/health/workouts/components/workout-form/workout-form.component.ts
@@ -16,13 +16,52 @@ import { SimpleChange } from '@angular/core/src/change_detection/change_detectio
                         <h3>Workout name</h3>
                         <input
                             type="text"
-                            placeholder="e.g. Breakfast"
+                            [placeholder]="placeholder"
                             formControlName="name">
                         <div class="error" *ngIf="required">
                             Workout name is required
                         </div>
                     </label>
-                </div>                
+                    <label>
+                        <h3>Type</h3>
+                        <workout-type formControlName="type">
+                        </workout-type>
+                    </label>
+                </div> 
+                
+                <div class="workout-form__details">
+                    <div *ngIf="form.get('type').value === 'strength'">
+                        <div class="workout-form__fields"
+                            formGroupName="strength">
+                            <label>
+                                <h3>Reps</h3>
+                                <input type="number" formControlName="reps">
+                            </label>
+                            <label>
+                                <h3>Sets</h3>
+                                <input type="number" formControlName="sets">
+                            </label>
+                            <label>
+                                <h3>Weight</h3>
+                                <input type="number" formControlName="weight">
+                            </label>
+                        </div>
+                    </div>
+                    <div *ngIf="form.get('type').value === 'endurance'">
+                        <div class="workout-form__fields"
+                            formGroupName="endurance">
+                            <label>
+                                <h3>Distance <span>(km)</span></h3>
+                                <input type="number" formControlName="distance">
+                            </label>
+                            <label>
+                                <h3>Duration <span>(minutes)</span></h3>
+                                <input type="number" formControlName="duration">
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="workout-form__submit">
                     <div>
                         <button *ngIf="!exists"
@@ -82,15 +121,33 @@ export class WorkoutFormComponent implements OnChanges {
     @Output() remove = new EventEmitter<Workout>();
 
     form = this.fb.group({
-        name: ['', Validators.required]
+        name: ['', Validators.required],
+        type: 'strength',
+        strength: this.fb.group({
+            reps: 0,
+            sets: 0,
+            weight: 0
+        }),
+        endurance: this.fb.group({
+            distance: 0,
+            duration: 0
+        })
     });
     
     constructor(
         private fb: FormBuilder
     ) {}
 
+    get placeholder() {
+        return `e.g. ${this.form.get('type').value === 'strength' ? 'Benchpress' : 'Treadmill' }`;
+    }
+
     ngOnChanges(changes: SimpleChanges) {
-       
+       if(this.workout && this.workout.name) {
+           this.exists = true;
+           const value = this.workout;
+           this.form.patchValue(value);
+       }
     }
 
     // get ingredients() {
@@ -102,12 +159,6 @@ export class WorkoutFormComponent implements OnChanges {
             this.form.get('name').hasError('required') && this.form.get('name').touched
         );
     }
-
-    // emptyIngredients() {
-    //     while(this.ingredients.controls.length) {
-    //         this.ingredients.removeAt(0);
-    //     }
-    // }
 
     createWorkout() {
         if (this.form.valid) {            
@@ -124,14 +175,6 @@ export class WorkoutFormComponent implements OnChanges {
     removeWorkout() {
         this.remove.emit(this.form.value);
     }
-
-    // addIngredient() {
-    //     this.ingredients.push(new FormControl(['']));
-    // }
-
-    // removeIngredient(index: number) {
-    //     this.ingredients.removeAt(index);
-    // }
 
     toggle() {
         this.toggled = !this.toggled;
